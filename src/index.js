c4c7aa69f0156bceccff4208628a1bdd58112f71
Daron Wiafe <daronkwiafe@gmail.com>
2022-10-12 21:00:46 +0200

    Send request to API to store and retrieve data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 // eslint-disable-next-line
 import _ from 'lodash';
-import scoreData from './modules/data';
+import getInfo from './modules/dataGet';
+import postInfo from './modules/dataSend';
+import { 
+  nameInput, 
+  scoreInput } from './modules/data';
 import dynamicInput from './modules/execution';
 import './styles.css';
 
-dynamicInput(scoreData);
+
+document.querySelectorAll('.btn')[1].addEventListener('click', () => {
+  const data = {
+    user: nameInput.value,
+    score: scoreInput.value,
+  };
+  
+  postInfo('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lo7BETKEBv4khCfPtazj/scores', data);
+
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+  document.querySelector('.scores-data').innerHTML = '';
+  
+  getInfo()
+    .then(response => response.json())
+    .then(data => dynamicInput(data.result))
+})
--- src/modules/execution.js
+++ src/modules/execution.js
@@ -1,11 +1,7 @@
-// import { scoreData } from "./data";
-
-// const uList = document.querySelector('.scores-data');
-
 const dynamicInput = (arr) => {
   arr.forEach((data) => {
     document.querySelector('.scores-data').innerHTML += `
-    <li class="score-info">${data.name}: ${data.score}</li>
+    <li class="score-info">${data.user}: ${data.score}</li>
     `;
   });
 };
