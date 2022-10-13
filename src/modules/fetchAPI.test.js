bf44075421a00cefb890a2023c87a9cec31ea8de
Daron Wiafe <daronkwiafe@gmail.com>
2022-10-13 09:02:16 +0200

    Alter fetch test

--- src/modules/fetchAPI.test.js
+++ src/modules/fetchAPI.test.js
@@ -1,8 +1,6 @@
 /* eslint-disable */ 
 
-import {jest} from '@jest/globals';
 import getInfo from './dataGet';
-import postInfo from "./dataSend";
 
 const dataToBeRecieved = {
     name: 'jim',
@@ -21,19 +19,6 @@ global.fetch = jest.fn((url, data) => {
       }
     }
   })
-})
-
-test('send data', () => {
-
-  const testData = {
-    name: 'jim',
-    score: 20
-  }
-
-  const execute = postInfo('http//:dummytestdata/game/score',testData);
-
-  expect(execute).resolves.toEqual(dataToBeRecieved);
-
 })
 
 test('retrieve data', () => {
